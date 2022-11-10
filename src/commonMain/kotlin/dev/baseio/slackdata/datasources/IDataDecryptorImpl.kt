0dc64804b169cc19140cf1be6e6a0bbfc07ee99e
Anmol Verma <anmol.verma4@gmail.com>
2022-11-10 20:38:18 +0530

    fix: enc decryption works for 1:1 users

--- src/commonMain/kotlin/dev/baseio/slackdata/datasources/IDataDecryptorImpl.kt
+++ src/commonMain/kotlin/dev/baseio/slackdata/datasources/IDataDecryptorImpl.kt
@@ -4,10 +4,6 @@ import dev.baseio.security.RsaEcdsaKeyManager
 import dev.baseio.slackdomain.datasources.IDataDecryptor
 
 class IDataDecryptorImpl(private val keyManager: RsaEcdsaKeyManager) : IDataDecryptor {
-    init {
-        keyManager.rawGenerateKeyPair()
-    }
-
     override fun decrypt(byteArray: ByteArray): ByteArray {
         return keyManager.decrypt(byteArray, keyManager.getPrivateKey())
     }
--- src/commonMain/kotlin/dev/baseio/slackdata/datasources/IDataEncrypterImpl.kt
+++ src/commonMain/kotlin/dev/baseio/slackdata/datasources/IDataEncrypterImpl.kt
@@ -4,11 +4,7 @@ import dev.baseio.security.RsaEcdsaKeyManager
 import dev.baseio.slackdomain.datasources.IDataEncrypter
 
 class IDataEncrypterImpl(private val keyManager: RsaEcdsaKeyManager) : IDataEncrypter {
-    init {
-        keyManager.rawGenerateKeyPair()
-    }
-
     override fun encrypt(byteArray: ByteArray, publicKeyBytes: ByteArray): ByteArray {
-        return keyManager.encrypt(byteArray, keyManager.getPublicKey())
+        return keyManager.encrypt(byteArray, keyManager.getPublicKeyFromBytes(publicKeyBytes))
     }
 }

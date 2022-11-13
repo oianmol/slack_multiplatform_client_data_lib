5827e73a181c4d234de9f8fb9601568b82ee25d3
anmol verma <anmol.verma4@gmail.com>
2022-11-13 14:09:53 +0530

    wip: for adding support for iOS main for java.security methods

--- src/commonMain/kotlin/dev/baseio/slackdata/datasources/IDataDecryptorImpl.kt
+++ src/commonMain/kotlin/dev/baseio/slackdata/datasources/IDataDecryptorImpl.kt
@@ -1,17 +1,16 @@
 package dev.baseio.slackdata.datasources
 
 import dev.baseio.security.HybridRsaUtils
-import dev.baseio.security.RsaEcdsaConstants
+import dev.baseio.security.OAEPParameterSpec
+import dev.baseio.security.Padding
 import dev.baseio.slackdomain.datasources.IDataDecryptor
-import java.security.KeyFactory
-import java.security.spec.PKCS8EncodedKeySpec
 
 class IDataDecryptorImpl : IDataDecryptor {
   override fun decrypt(byteArray: ByteArray, privateKeyBytes: ByteArray): ByteArray {
     val spec = PKCS8EncodedKeySpec(privateKeyBytes)
     val kf = KeyFactory.getInstance("RSA")
     return HybridRsaUtils.decrypt(
-      byteArray, kf.generatePrivate(spec), RsaEcdsaConstants.Padding.OAEP, RsaEcdsaConstants.OAEP_PARAMETER_SPEC
+      byteArray, kf.generatePrivate(spec), Padding.OAEP, OAEPParameterSpec().oaepParamSpec
     )
   }
 

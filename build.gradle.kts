825fe1e827e32f33dde59203fb3799c8a25049bd
anmol verma <anmol.verma4@gmail.com>
2022-11-21 23:10:28 +0530

    setup: native iosApp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
 import io.github.timortel.kotlin_multiplatform_grpc_plugin.GrpcMultiplatformExtension.OutputTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 
 plugins {
-  kotlin("multiplatform")
-  kotlin("plugin.serialization") version "1.7.20"
-  id("com.android.library")
-  kotlin("native.cocoapods")
-  id("com.google.protobuf") version "0.8.19"
-  id("com.squareup.sqldelight")
-  id("maven-publish")
-  id("io.github.timortel.kotlin-multiplatform-grpc-plugin") version "0.2.2"
+    kotlin("multiplatform")
+    kotlin("plugin.serialization") version "1.7.20"
+    id("com.android.library")
+    kotlin("native.cocoapods")
+    id("com.google.protobuf") version "0.8.19"
+    id("com.squareup.sqldelight")
+    id("maven-publish")
+    id("io.github.timortel.kotlin-multiplatform-grpc-plugin") version "0.2.2"
 }
 
 group = "dev.baseio.slackclone"
@@ -19,178 +20,181 @@ version = "1.0"
 val ktor_version = "2.1.0"
 
 object Jvm {
-  val target = JavaVersion.VERSION_1_8
+    val target = JavaVersion.VERSION_1_8
 }
 
 repositories {
-  mavenCentral()
-  mavenLocal()
-  google()
+    mavenCentral()
+    mavenLocal()
+    google()
 }
 
 kotlin {
-  jvm {
-    compilations.all {
-      kotlinOptions.jvmTarget = "11"
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
     }
-  }
-  android() {
-    publishLibraryVariants("release")
-  }
-  iosX64()
-  iosArm64()
-  iosSimulatorArm64()
-
-  cocoapods {
-    summary = "Some description for the Shared Module"
-    homepage = "Link to the Shared Module homepage"
-    ios.deploymentTarget = "14.1"
-    podfile = project.file("../iosApp/Podfile")
-    framework {
-      baseName = "slack_data_layer"
-    }
-  }
-
-  sourceSets {
-    val commonMain by getting {
-      dependencies {
-        implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
-        implementation("com.squareup.sqldelight:runtime:1.5.3")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
-        implementation(Deps.Kotlinx.datetime)
-        implementation(Deps.SqlDelight.runtime)
-        implementation(Lib.Async.COROUTINES)
-        implementation(project(Lib.Project.SLACK_DOMAIN_COMMON))
-        implementation(kotlin("stdlib-common"))
-        api("io.github.timortel:grpc-multiplatform-lib:0.2.2")
-      }
-      kotlin.srcDirs(
-        projectDir.resolve("build/generated/source/kmp-grpc/commonMain/kotlin").canonicalPath,
-      )
-    }
-    val sqlDriverNativeMain by creating {
-      dependsOn(commonMain)
-      dependencies {
-        implementation("com.squareup.sqldelight:native-driver:1.5.3")
-      }
-    }
-    val commonTest by getting {
-      dependencies {
-        implementation(kotlin("test"))
-      }
-    }
-    val androidMain by getting {
-      kotlin.srcDirs(
-        projectDir.resolve("build/generated/source/kmp-grpc/jvmMain/kotlin").canonicalPath,
-      )
-      dependencies {
-        implementation(Deps.SqlDelight.androidDriver)
-        implementation("androidx.security:security-crypto-ktx:1.1.0-alpha04")
-        api(project(":slack_generate_protos"))
-        implementation("io.ktor:ktor-client-android:$ktor_version")
-      }
-    }
-    val iosArm64Main by getting {
-      dependsOn(sqlDriverNativeMain)
-      dependencies {
-        implementation(Deps.Kotlinx.IOS.coroutinesArm64)
-      }
-    }
-    val iosSimulatorArm64Main by getting {
-      dependsOn(sqlDriverNativeMain)
-      dependencies {
-        implementation(Deps.Kotlinx.IOS.coroutinesiossimulatorarm64)
-      }
-    }
-    val iosX64Main by getting {
-      dependsOn(sqlDriverNativeMain)
-      dependencies {
-        implementation(Deps.Kotlinx.IOS.coroutinesX64)
-      }
+    android() {
+        publishLibraryVariants("release")
     }
 
-    val iosMain by creating {
-      kotlin.srcDirs(
-        projectDir.resolve("build/generated/source/kmp-grpc/iosMain/kotlin").canonicalPath,
-      )
-      dependencies {
-        implementation("com.squareup.sqldelight:native-driver:1.5.3")
-        implementation("io.ktor:ktor-client-darwin:$ktor_version")
-      }
-      dependsOn(commonMain)
-      iosX64Main.dependsOn(this)
-      iosArm64Main.dependsOn(this)
-      iosSimulatorArm64Main.dependsOn(this)
-    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
 
-    val androidTest by getting {
-      dependencies {
-        implementation("junit:junit:4.13.2")
-      }
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        version = "1.0"
+        ios.deploymentTarget = "14.1"
+        podfile = project.file("../iosApp/Podfile")
+        framework {
+            baseName = "slack_data_layer"
+        }
     }
-    val jvmMain by getting {
-      kotlin.srcDirs(
-        projectDir.resolve("build/generated/source/kmp-grpc/jvmMain/kotlin").canonicalPath,
-      )
-      dependencies {
-        implementation(Deps.Kotlinx.JVM.coroutinesSwing)
-        implementation(Deps.SqlDelight.jvmDriver)
-        api(project(":slack_generate_protos"))
-        implementation("io.ktor:ktor-client-java:$ktor_version")
-      }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation("com.squareup.sqldelight:runtime:1.5.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+                implementation(Deps.Kotlinx.datetime)
+                implementation(Deps.SqlDelight.runtime)
+                implementation(Lib.Async.COROUTINES)
+                implementation(project(Lib.Project.SLACK_DOMAIN_COMMON))
+                implementation(kotlin("stdlib-common"))
+                api("io.github.timortel:grpc-multiplatform-lib:0.2.2")
+            }
+            kotlin.srcDirs(
+                projectDir.resolve("build/generated/source/kmp-grpc/commonMain/kotlin").canonicalPath,
+            )
+        }
+        val sqlDriverNativeMain by creating {
+            dependsOn(commonMain)
+            dependencies {
+                implementation("com.squareup.sqldelight:native-driver:1.5.3")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting {
+            kotlin.srcDirs(
+                projectDir.resolve("build/generated/source/kmp-grpc/jvmMain/kotlin").canonicalPath,
+            )
+            dependencies {
+                implementation(Deps.SqlDelight.androidDriver)
+                implementation("androidx.security:security-crypto-ktx:1.1.0-alpha04")
+                api(project(":slack_generate_protos"))
+                implementation("io.ktor:ktor-client-android:$ktor_version")
+            }
+        }
+        val iosArm64Main by getting {
+            dependsOn(sqlDriverNativeMain)
+            dependencies {
+                implementation(Deps.Kotlinx.IOS.coroutinesArm64)
+            }
+        }
+        val iosSimulatorArm64Main by getting {
+            dependsOn(sqlDriverNativeMain)
+            dependencies {
+                implementation(Deps.Kotlinx.IOS.coroutinesiossimulatorarm64)
+            }
+        }
+        val iosX64Main by getting {
+            dependsOn(sqlDriverNativeMain)
+            dependencies {
+                implementation(Deps.Kotlinx.IOS.coroutinesX64)
+            }
+        }
+
+        val iosMain by creating {
+            kotlin.srcDirs(
+                projectDir.resolve("build/generated/source/kmp-grpc/iosMain/kotlin").canonicalPath,
+            )
+            dependencies {
+                implementation("com.squareup.sqldelight:native-driver:1.5.3")
+                implementation("io.ktor:ktor-client-darwin:$ktor_version")
+            }
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+
+
+        val androidTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13.2")
+            }
+        }
+        val jvmMain by getting {
+            kotlin.srcDirs(
+                projectDir.resolve("build/generated/source/kmp-grpc/jvmMain/kotlin").canonicalPath,
+            )
+            dependencies {
+                implementation(Deps.Kotlinx.JVM.coroutinesSwing)
+                implementation(Deps.SqlDelight.jvmDriver)
+                api(project(":slack_generate_protos"))
+                implementation("io.ktor:ktor-client-java:$ktor_version")
+            }
+        }
     }
-  }
 }
 
 grpcKotlinMultiplatform {
-  targetSourcesMap.put(OutputTarget.COMMON, listOf(kotlin.sourceSets.getByName("commonMain")))
-  targetSourcesMap.put(
-    OutputTarget.JVM,
-    listOf(kotlin.sourceSets.getByName("jvmMain"), kotlin.sourceSets.getByName("androidMain"))
-  )
-  targetSourcesMap.put(
-    OutputTarget.IOS,
-    listOf(
-      kotlin.sourceSets.getByName("iosArm64Main"),
-      kotlin.sourceSets.getByName("iosSimulatorArm64Main"),
-      kotlin.sourceSets.getByName("iosX64Main")
+    targetSourcesMap.put(OutputTarget.COMMON, listOf(kotlin.sourceSets.getByName("commonMain")))
+    targetSourcesMap.put(
+        OutputTarget.JVM,
+        listOf(kotlin.sourceSets.getByName("jvmMain"), kotlin.sourceSets.getByName("androidMain"))
     )
-  )
-  //Specify the folders where your proto files are located, you can list multiple.
-  protoSourceFolders.set(listOf(projectDir.parentFile.resolve("slack_protos/src/main/proto")))
+    targetSourcesMap.put(
+        OutputTarget.IOS,
+        listOf(
+            kotlin.sourceSets.getByName("iosArm64Main"),
+            kotlin.sourceSets.getByName("iosSimulatorArm64Main"),
+            kotlin.sourceSets.getByName("iosX64Main")
+        )
+    )
+    //Specify the folders where your proto files are located, you can list multiple.
+    protoSourceFolders.set(listOf(projectDir.parentFile.resolve("slack_protos/src/main/proto")))
 }
 
 dependencies {
-  commonMainApi("dev.baseio.slackcrypto:capillary-kmp:1.0")
+    commonMainApi("dev.baseio.slackcrypto:capillary-kmp:1.0")
 }
 
 kotlin {
-  targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
-    binaries.all {
-      // TODO: the current compose binary surprises LLVM, so disable checks for now.
-      freeCompilerArgs += "-Xdisable-phases=VerifyBitcode"
+    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+        binaries.all {
+            // TODO: the current compose binary surprises LLVM, so disable checks for now.
+            freeCompilerArgs += "-Xdisable-phases=VerifyBitcode"
+        }
     }
-  }
 }
 
 sqldelight {
-  database("SlackDB") {
-    packageName = "dev.baseio.database"
-    linkSqlite = true
-  }
+    database("SlackDB") {
+        packageName = "dev.baseio.database"
+        linkSqlite = true
+    }
 }
 
 
 android {
-  compileSdk = 33
-  sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-  defaultConfig {
-    minSdk = (24)
-    targetSdk = (33)
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
+    compileSdk = 33
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = (24)
+        targetSdk = (33)
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }

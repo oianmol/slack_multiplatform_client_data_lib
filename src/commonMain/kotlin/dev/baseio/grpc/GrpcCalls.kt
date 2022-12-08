2088a45dbbf231d53c36870a5d1cae8c7b3a519e
anmol verma <anmol.verma4@gmail.com>
2022-12-08 11:45:59 +0530

    add: android and desktop with deep link implemented!

--- src/commonMain/kotlin/dev/baseio/grpc/GrpcCalls.kt
+++ src/commonMain/kotlin/dev/baseio/grpc/GrpcCalls.kt
@@ -93,7 +93,7 @@ class GrpcCalls(
     override suspend fun sendMagicLink(
         workspace: KMSKCreateWorkspaceRequest,
         token: String?
-    ): KMEmpty {
+    ): KMSKWorkspace {
         return workspacesStub.letMeIn(workspace, fetchToken(token))
     }
 
@@ -274,7 +274,7 @@ interface IGrpcCalls {
     suspend fun sendMagicLink(
         workspace: KMSKCreateWorkspaceRequest,
         token: String? = skKeyValueData.get(AUTH_TOKEN)
-    ): KMEmpty
+    ): KMSKWorkspace
 
     suspend fun getPublicChannels(
         workspaceIdentifier: String,
